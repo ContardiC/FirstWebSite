e2db1e98fcb69c3aaca5c5d11b982b79a867a57a
Carlo Contardi <c.contardi@icloud.com>
2020-10-27 09:10:43 +0100

    SESSIONS

--- src/login.php
+++ src/login.php
@@ -1,7 +1,8 @@
 <?php
-
+session_start(); // sempre la prima funzione
 $email=$_POST['email'];
 $password=$_POST['password'];
+
 // echo $email."<br>".$password;
 $file=fopen("data/users.txt","r") or die("Unable to open file!");
 $i=0;
@@ -15,4 +16,10 @@ while($i<count($arr)){
     echo $arr[$i];
     $i++;
 }
+// controllo credenziali
+// se le credenziali sono corrette
+//email && password checked
+$_SESSION['email']=$email; // un campo univoco 
+// esempio email o ID
+
 fclose($file);
